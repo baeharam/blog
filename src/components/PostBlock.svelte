51e216dbae6f3942750ee91481b2a528883d1634
baeharam <unknownfrontenddeveloper@gmail.com>
2022-08-29 22:53:55 +0900

    Setup post block and post rendering

---
+++ src/components/PostBlock.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  export let date: string;
+  export let title: string;
+  export let description: string;
+  export let path: string;
+</script>
+
+<div class="flex flex-col">
+  <span class="text-slate-400 text-sm mb-4">{date}</span>
+  <a
+    href={path}
+    class="text-4xl font-bold text-strong-blue mb-3 cursor-pointer"
+  >
+    {title}
+  </a>
+  <p class="text-base">{description}</p>
+</div>
