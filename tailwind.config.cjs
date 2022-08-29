9972938695abdd79f88b9ccb47c07d6cc60bf7f6
baeharam <unknownfrontenddeveloper@gmail.com>
2022-08-29 20:58:27 +0900

    Setup tailwind CSS

---
+++ tailwind.config.cjs
@@ -0,0 +1,12 @@
+module.exports = {
+  content: [
+    './public/**/*.html',
+    './src/**/*.{astro,md,js,jsx,svelte,ts,tsx,vue}',
+    '../../libs/**/*.{astro,md,js,jsx,svelte,ts,tsx,vue}',
+  ],
+  theme: {
+    fontFamily: {
+      sans: ["Autograf PERSONAL USE ONLY"]
+    }
+  }
+}
