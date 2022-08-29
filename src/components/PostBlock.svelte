0b962154c6e6fd4cf74c824fb793269865cf2b70
baeharam <unknownfrontenddeveloper@gmail.com>
2022-08-29 22:59:16 +0900

    Format post date with dayjs

--- src/components/PostBlock.svelte
+++ src/components/PostBlock.svelte
@@ -1,12 +1,16 @@
 <script lang="ts">
-  export let date: string;
+  import dayjs from "dayjs";
+
+  export let date: Date;
   export let title: string;
   export let description: string;
   export let path: string;
 </script>
 
 <div class="flex flex-col">
-  <span class="text-slate-400 text-sm mb-4">{date}</span>
+  <span class="text-slate-400 text-sm mb-4"
+    >{dayjs(date).format("YYYY년 MM월 DD일")}</span
+  >
   <a
     href={path}
     class="text-4xl font-bold text-strong-blue mb-3 cursor-pointer"
