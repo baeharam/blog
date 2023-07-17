1792afbb0deef820988dc4fe303e0fbf90b0bd0f
baeharam <unknownfrontenddeveloper@gmail.com>
2023-07-17 20:37:01 +0900

    feat: Change default font

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -8,7 +8,8 @@ module.exports = {
   ],
   theme: {
     fontFamily: {
-      sans: ["Nanum Gothic"],
+      sans: ["Youth"],
+      logo: ["Autograf"],
     },
     extend: {
       colors: {
@@ -24,7 +25,12 @@ module.exports = {
     plugin(function ({ addBase }) {
       addBase({
         "@font-face": {
-          fontFamily: "'Autograf'",
+          fontFamily: "Youth",
+          src: "url(//cdn.jsdelivr.net/korean-webfonts/1/orgs/othrs/kywa/Youth/Youth.woff2) format('woff2'), url(//cdn.jsdelivr.net/korean-webfonts/1/orgs/othrs/kywa/Youth/Youth.woff) format('woff')",
+        },
+
+        "@font-face": {
+          fontFamily: "Autograf",
           src: "url(/fonts/Autograf.woff) format('woff')",
         },
       });
